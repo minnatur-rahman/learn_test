d912b64cc219b085cbf4bc3abd92cd7bf50447d7
millatur <minnatur7373@gmail.com>
2024-02-17 21:36:10 +0600

    second mojit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind('first_service_provider',function($app){
+             dd('This is my first service helper');
+        });
+
+        app()->bind('second_service_provider',function($app){
+            dd('This is my second service container');
+       });
+
+       app()->bind('third_service_provider',function($app){
+        dd('This is my third service container');
+   });
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // dd(app());
     return view('welcome');
 });
+
+Route::get('/test',function(){
+
+    app()->make('first_service_helper');
+
+});
+
+Route::get('/test-one',function(){
+
+
+
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
