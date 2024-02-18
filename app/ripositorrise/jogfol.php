35b356d62a3a3a97bfd26aa481e32fe7564c62e2
millatur <minnatur7373@gmail.com>
2024-02-18 23:21:14 +0600

    edit some data

---
+++ app/ripositorrise/jogfol.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\ripositorrise;
+
+class jogfol{
+    public function test(){
+        $a=10;
+        $b=20;
+        $c=$a+$b;
+
+    }
+}
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // dd(app());
     return view('welcome');
 });
 
+
+
 Route::get('/test',function(){
-
     app()->make('first_service_helper');
-
 });
 
 Route::get('/test-one',function(){
