c3ee20afb8a5752f2c0a4fa82a02bafd05d28c16
millatur <minnatur7373@gmail.com>
2024-02-21 02:53:09 +0600

    controller parctice

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\FirstController;
+use App\Http\Controllers\LearnController;
 use App\Http\Controllers\ProfileController;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', LearnController::class);
 
 
 
