3a06facefd04a9a019ddb70bb1cbe9a7b2855395
millatur <minnatur7373@gmail.com>
2024-02-26 21:50:38 +0600

    edit some data

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,24 +8,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="row-card">
-        <div class="card-body col-lg-6">
-            <form>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="card col-lg-6 ml-4">
+            <div class="card-body">
+              <form method="POST" action="">
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1">
-
+                  <input type="email" name="name" class="form-control">
+                </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Email Address</label>
-                  <input type="password" name="namne" class="form-control" id="exampleInputPassword1">
+                  <input type="email" name="email" class="form-control">
                 </div>
-                <div class="mb-3 form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
+            </div>
         </div>
+    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
